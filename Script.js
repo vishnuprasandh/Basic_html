5bc7e82a223ab318158bbd5bad2201ac35242257
vishnuprasandh <141708981+vishnuprasandh@users.noreply.github.com>
2023-08-09 11:42:17 +0530

    basic html

---
+++ Script.js
@@ -0,0 +1,23 @@
+function createParagraph() {
+    const para = document.createElement("p");
+    para.textContent = "You clicked the button!";
+    document.body.appendChild(para);
+  }
+  
+  const buttons = document.querySelectorAll("button");
+  
+  for (const button of buttons) {
+    button.addEventListener("click", createParagraph);
+  }
+  
+// function createParagraph() {
+//     const para = document.createElement('p');
+//     para.textContent = 'You clicked the button!';
+//     document.body.appendChild(para);
+//   }
+  
+//   const buttons = document.querySelectorAll('button');
+  
+//   for (const button of buttons) {
+//     button.addEventListener('click', createParagraph);
+//   }
